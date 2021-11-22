b78e6552c9a5f2cf3d910be2dc5f434f943f4370
Alexander Chkliar <alex.chkliar@gmail.com>
2021-11-22 17:38:37 -0500

    get lowercase in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   resources :users, only: [:index, :show] do
     resources :messages, only: [:index, :create]
   end
+
   resources :trails, only: [:index, :show] do
     resources :runs, only: [:new, :create]
   end
 
-  GET "/messages", to: "messages#all_messages", as: :all_messages
-  GET "/my_runs", to: "runs#my_runs", as: :my_runs
-  GET "/my_runs/:id", to: "runs#my_run", as: :my_run
+  get "/messages", to: "messages#all_messages", as: :all_messages
+  get "/my_runs", to: "runs#my_runs", as: :my_runs
+  get "/my_runs/:id", to: "runs#my_run", as: :my_run
 end
