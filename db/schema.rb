f5fb2874f59d659593844c1a1212ca93a71a3fb3
Alexander Chkliar <alex.chkliar@gmail.com>
2021-11-22 17:53:11 -0500

    some changes to migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_213244) do
+ActiveRecord::Schema.define(version: 2021_11_22_220619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "sender_id", null: false
+    t.bigint "receiver_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["receiver_id"], name: "index_messages_on_receiver_id"
+    t.index ["sender_id"], name: "index_messages_on_sender_id"
+  end
+
+  create_table "run_participants", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "run_id", null: false
+    t.boolean "creator"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["run_id"], name: "index_run_participants_on_run_id"
+    t.index ["user_id"], name: "index_run_participants_on_user_id"
+  end
+
+  create_table "runs", force: :cascade do |t|
+    t.bigint "trail_id", null: false
+    t.boolean "status"
+    t.datetime "date"
+    t.integer "duration"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["trail_id"], name: "index_runs_on_trail_id"
+  end
+
+  create_table "trails", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "distance"
+    t.integer "difficulty"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_trails_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +68,10 @@ ActiveRecord::Schema.define(version: 2021_11_22_213244) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "users", column: "receiver_id"
+  add_foreign_key "messages", "users", column: "sender_id"
+  add_foreign_key "run_participants", "runs"
+  add_foreign_key "run_participants", "users"
+  add_foreign_key "runs", "trails"
+  add_foreign_key "trails", "users"
 end
