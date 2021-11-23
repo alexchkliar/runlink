f731575c165254a816b7ea50e4795e7de47a10e8
Alexander Chkliar <alex.chkliar@gmail.com>
2021-11-23 13:59:59 -0500

    run booking WIP

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,14 +1,31 @@
 class RunsController < ApplicationController
   def new
     @trail = Trail.find(params[:trail_id])
+    @users = User.all
   end
 
   def create
+    @run = Run.new(run_params)
+    # @booking.user = current_user
+
+    @watch = Watch.find(params[:watch_id])
+    @booking.watch = @watch
+    authorize(@booking)
+    if @booking.save
+      redirect_to my_bookings_path, notice: 'Booking was successfully created.'
+    else
+      render :new
+    end
   end
 
   def my_runs
   end
 
   def my_run
+  end
+
+  private
+  def run_params
+    params.require(:booking).permit(:trail_id, :status, :start)
   end
 end
