046506984c1eaab7202f2bd9685a7b2b637eedf6
Joon Ho Shin <joonhoshin2021@gmail.com>
2021-11-23 12:07:53 -0500

    users_controller index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    # authorize(@users)
   end
 
   def show
+    @user = User.find(params[:id])
+    authorize(@user)
+  end
+
+  def user_params
+    params.require(:user).permit(:id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@ User.destroy_all
 lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
 puts "Creating users..."
-apu = User.create!(first_name: 'Apu', last_name: 'Nahasapeemapetilon', email: "a@a.a", password: "123456", gender: '', location: '', running_exp: 'Beginner', bio: lorem, last_name: '', birth_date: (Date.current - (365 * 20)))
-bob = User.create!(first_name: 'Bob', last_name: 'Smith', email: "b@b.b", password: "123456", gender: '', location: '', running_exp: 'Intermediate', bio: lorem, last_name: '', birth_date: (Date.current - (365 * 20)))
-chung = User.create!(first_name: 'Chung', last_name: 'Wing', email: "c@c.c", password: "123456", gender: '', location: '', running_exp: 'Advanced', bio: lorem, last_name: '', birth_date: (Date.current - (365 * 20)))
+apu = User.create!(first_name: 'Apu', last_name: 'Nahasapeemapetilon', email: "a@a.a", password: "123456", gender: 'male', location: 'Montreal', running_exp: 'Beginner', bio: lorem, birth_date: (Date.current - (365 * 20)))
+bob = User.create!(first_name: 'Bob', last_name: 'Smith', email: "b@b.b", password: "123456", gender: '', location: '', running_exp: 'Intermediate', bio: lorem, birth_date: (Date.current - (365 * 20)))
+chung = User.create!(first_name: 'Chung', last_name: 'Wing', email: "c@c.c", password: "123456", gender: '', location: '', running_exp: 'Advanced', bio: lorem, birth_date: (Date.current - (365 * 20)))
