8e45bf5ed99b1ba09f0d71c6dacf677a75916b73
Alexander Chkliar <alex.chkliar@gmail.com>
2021-11-29 23:38:27 -0500

    map and formatting changes

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -2,7 +2,7 @@ import "jquery-bar-rating";
 import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuery section)
 
 const initStarRating = () => {
-  $('#run_participant_trail_2').barrating({
+  $('#trail-star-target').barrating({
     theme: 'css-stars'
   });
   $('#run_participant_difficulty').barrating({
