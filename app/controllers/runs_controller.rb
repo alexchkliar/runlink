8ec86815e36616387da106c08161a1bec10e6aef
Alex Chkliar <alex.chkliar@gmail.com>
2021-12-02 11:10:35 -0500

    booking run date validation fix

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,15 +1,17 @@
 class RunsController < ApplicationController
   def new
     @trail = Trail.find(params[:trail_id])
-    recipients_in_chats_ids1 = Chatroom.all.where(user_id: current_user).map { |chatroom| chatroom.recipient_id }
-    recipients_in_chats_ids2 = Chatroom.all.where(recipient_id: current_user).map { |chatroom| chatroom.recipient_id }
-    recipients_in_chats_ids = (recipients_in_chats_ids1 + recipients_in_chats_ids2)
-    @users = recipients_in_chats_ids.map { |user_id| User.find(user_id) unless User.find(user_id).first_name.nil? || user_id == current_user }
+    # recipients_in_chats_ids1 = Chatroom.all.where(user_id: current_user).map { |chatroom| chatroom.recipient_id }
+    # recipients_in_chats_ids2 = Chatroom.all.where(recipient_id: current_user).map { |chatroom| chatroom.recipient_id }
+    # recipients_in_chats_ids = (recipients_in_chats_ids1 + recipients_in_chats_ids2)
+    # @users = recipients_in_chats_ids.map { |user_id| User.find(user_id) unless User.find(user_id).first_name.nil? || user_id == current_user }
+    @running_partner = User.find(params[:running_partner].to_i)
     @run = Run.new
     @run.run_participants.build
   end
 
   def create
+    @running_partner = User.find(params[:run][:run_participants_attributes]["0"][:user_id].to_i)
     @run = Run.new(run_params)
     @trail = Trail.find(params[:trail_id])
     @run.trail = @trail
