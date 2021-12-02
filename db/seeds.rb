830c5d507515814adc27b2589df8b69a004502a9
Leonardo Van Schaik <leonardo.vanschaik@gmail.com>
2021-12-02 15:44:23 -0500

    seed sdjustment

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ Trail.destroy_all
 puts "Creating users..."
 
 file = URI.open('https://res.cloudinary.com/davidshin0413/image/upload/v1638462036/chung_fatorj.jpg')
-chung = User.new(first_name: 'Chung', last_name: 'Ma', email: "a@a.a", password: "123456", location: 'Plateau Mont-Royal', running_exp: 'Beginner', bio: chung_bio, birth_date: (Date.current - (365 * 20)), xp: 5)
+chung = User.new(first_name: 'Chung', last_name: 'Ma', email: "a@a.a", password: "123456", location: 'Plateau Mont-Royal', running_exp: 'Beginner', bio: chung_bio, birth_date: (Date.current - (365 * 20)), xp: 0)
 chung.avatar.attach(io: file, filename: 'chung_avatar.jpg', content_type: 'image/jpg')
 chung.save!
 
@@ -32,17 +32,17 @@ don.avatar.attach(io: file, filename: 'don_avatar.jpg', content_type: 'image/jpg
 don.save!
 
 file = URI.open('https://res.cloudinary.com/davidshin0413/image/upload/v1638462035/emily_cgy5ht.jpg')
-emily = User.new(first_name: 'Emily', last_name: 'Chadson', email: "c@c.c", password: "123456", location: 'Plateau Mont-Royal', running_exp: 'Advanced', bio: emily_bio, birth_date: (Date.current - (365 * 20)), xp: 1000)
+emily = User.new(first_name: 'Emily', last_name: 'Chadson', email: "c@c.c", password: "123456", location: 'Plateau Mont-Royal', running_exp: 'Advanced', bio: emily_bio, birth_date: (Date.current - (365 * 20)), xp: 10)
 emily.avatar.attach(io: file, filename: 'emily_avatar.jpg', content_type: 'image/jpg')
 emily.save!
 
 file = URI.open('https://res.cloudinary.com/davidshin0413/image/upload/v1638462036/fatima_vounaa.jpg')
-fatima = User.new(first_name: 'Fatima', last_name: 'DeVito', email: "d@d.d", password: "123456", location: 'Philadelphia', running_exp: 'Beginner', bio: fatima_bio, birth_date: (Date.current - (365 * 50)), xp: 20)
+fatima = User.new(first_name: 'Fatima', last_name: 'DeVito', email: "d@d.d", password: "123456", location: 'Philadelphia', running_exp: 'Beginner', bio: fatima_bio, birth_date: (Date.current - (365 * 50)), xp: 15)
 fatima.avatar.attach(io: file, filename: 'fatima_avatar.jpg', content_type: 'image/jpg')
 fatima.save!
 
 file = URI.open('https://res.cloudinary.com/davidshin0413/image/upload/v1638462035/issac_ixokxj.jpg')
-issac = User.new(first_name: 'Issac', last_name: 'DeVito', email: "e@e.e", password: "123456", location: 'Philadelphia', running_exp: 'Beginner', bio: issac_bio, birth_date: (Date.current - (365 * 50)), xp: 20)
+issac = User.new(first_name: 'Issac', last_name: 'DeVito', email: "e@e.e", password: "123456", location: 'Philadelphia', running_exp: 'Beginner', bio: issac_bio, birth_date: (Date.current - (365 * 50)), xp: 15)
 issac.avatar.attach(io: file, filename: 'issac_avatar.jpg', content_type: 'image/jpg')
 issac.save!
 
@@ -52,28 +52,28 @@ jessica.avatar.attach(io: file, filename: 'jessica_avatar.jpg', content_type: 'i
 jessica.save!
 
 file = URI.open('https://res.cloudinary.com/davidshin0413/image/upload/v1638462036/raj_m3g93m.jpg')
-raj = User.new(first_name: 'Raj', last_name: 'DeVito', email: "g@g.g", password: "123456", location: 'Philadelphia', running_exp: 'Beginner', bio: raj_bio, birth_date: (Date.current - (365 * 50)), xp: 20)
+raj = User.new(first_name: 'Raj', last_name: 'DeVito', email: "g@g.g", password: "123456", location: 'Philadelphia', running_exp: 'Beginner', bio: raj_bio, birth_date: (Date.current - (365 * 50)), xp: 4)
 raj.avatar.attach(io: file, filename: 'raj_avatar.jpg', content_type: 'image/jpg')
 raj.save!
 
 file = URI.open('https://res.cloudinary.com/davidshin0413/image/upload/v1638462035/shawnty_aemihc.jpg')
-shawnty = User.new(first_name: 'Shawnty', last_name: 'DeVito', email: "h@h.h", password: "123456", location: 'Philadelphia', running_exp: 'Beginner', bio: shawnty_bio, birth_date: (Date.current - (365 * 50)), xp: 20)
+shawnty = User.new(first_name: 'Shawnty', last_name: 'DeVito', email: "h@h.h", password: "123456", location: 'Philadelphia', running_exp: 'Beginner', bio: shawnty_bio, birth_date: (Date.current - (365 * 50)), xp: 6)
 shawnty.avatar.attach(io: file, filename: 'shawnty_avatar.jpg', content_type: 'image/jpg')
 shawnty.save!
 
 file = URI.open('https://res.cloudinary.com/davidshin0413/image/upload/v1638462035/tina_i2qegb.jpg')
-tina = User.new(first_name: 'Tina', last_name: 'DeVito', email: "i@i.i", password: "123456", location: 'Philadelphia', running_exp: 'Beginner', bio: tina_bio, birth_date: (Date.current - (365 * 50)), xp: 20)
+tina = User.new(first_name: 'Tina', last_name: 'DeVito', email: "i@i.i", password: "123456", location: 'Philadelphia', running_exp: 'Beginner', bio: tina_bio, birth_date: (Date.current - (365 * 50)), xp: 35)
 tina.avatar.attach(io: file, filename: 'tina_avatar.jpg', content_type: 'image/jpg')
 tina.save!
 
 file = URI.open('https://res.cloudinary.com/davidshin0413/image/upload/v1638462915/tyrone_muympy.jpg')
-tyrone = User.new(first_name: 'Tyrone', last_name: 'DeVito', email: "j@j.j", password: "123456", location: 'Philadelphia', running_exp: 'Beginner', bio: tyrone_bio, birth_date: (Date.current - (365 * 50)), xp: 20)
+tyrone = User.new(first_name: 'Tyrone', last_name: 'DeVito', email: "j@j.j", password: "123456", location: 'Philadelphia', running_exp: 'Beginner', bio: tyrone_bio, birth_date: (Date.current - (365 * 50)), xp: 14)
 tyrone.avatar.attach(io: file, filename: 'tyrone_avatar.jpg', content_type: 'image/jpg')
 tyrone.save!
 
 puts "Creating trails..."
 
-montroyal = Trail.new(name: "Mount Royal Park Loop", location: 'Mount Royal Park', distance: 8.4, user_id: apu.id, difficulty: 4, trail_img_url: "https://res.cloudinary.com/davidshin0413/image/upload/v1638221914/Screenshot_from_2021-11-29_16-20-33_y7knd0.png", description: "Mount Royal Park Loop is a 8.4 kilometer heavily trafficked loop trail that features a lake and is good for all skill levels. The trail offers a number of activity options and is accessible year-round.")
+montroyal = Trail.new(name: "Mount Royal Park Loop", location: 'Mount Royal Park', distance: 8.4, user_id: chung.id, difficulty: 4, trail_img_url: "https://res.cloudinary.com/davidshin0413/image/upload/v1638221914/Screenshot_from_2021-11-29_16-20-33_y7knd0.png", description: "Mount Royal Park Loop is a 8.4 kilometer heavily trafficked loop trail that features a lake and is good for all skill levels. The trail offers a number of activity options and is accessible year-round.")
 file = URI.open('http://site.ieee.org/pimrc-2017/files/2017/05/mount-royal-park-gomontrealtourism.com_.jpg')
 montroyal.photos.attach(io: file, filename: 'mount_royal_park_loop_photo.jpg', content_type: 'image/jpg')
 file = URI.open('https://cdn-assets.alltrails.com/uploads/photo/image/42408073/extra_large_40ea1b12fb757665b620ea5d620d6568.jpg')
@@ -83,7 +83,7 @@ montroyal.photos.attach(io: file, filename: 'mount_royal_park_loop_photo3.jpg', 
 montroyal.save!
 puts "Mount Royal saved..."
 
-westmount = Trail.new(name: "Westmount Loop", location: 'Westmount', distance: 3.1, user_id: apu.id, difficulty: 3, trail_img_url: "https://res.cloudinary.com/davidshin0413/image/upload/v1638222561/Screenshot_from_2021-11-29_16-22-55_mm2yf9.png", description: "Westmount Loop is a 3.1 kilometer lightly trafficked loop trail that offers the chance to see wildlife and is good for all skill levels. The trail is primarily used for hiking, walking, running, and nature trips.")
+westmount = Trail.new(name: "Westmount Loop", location: 'Westmount', distance: 3.1, user_id: chung.id, difficulty: 3, trail_img_url: "https://res.cloudinary.com/davidshin0413/image/upload/v1638222561/Screenshot_from_2021-11-29_16-22-55_mm2yf9.png", description: "Westmount Loop is a 3.1 kilometer lightly trafficked loop trail that offers the chance to see wildlife and is good for all skill levels. The trail is primarily used for hiking, walking, running, and nature trips.")
 file = URI.open('https://westmount.org/wp-content/uploads/2015/10/dog_walk_web.jpg')
 westmount.photos.attach(io: file, filename: 'west_mount_photo.jpg', content_type: 'image/jpg')
 file = URI.open('https://cdn-assets.alltrails.com/uploads/photo/image/30825090/extra_large_38df8ca4c6247b2175b1c0d61a414d8d.jpg')
@@ -93,7 +93,7 @@ westmount.photos.attach(io: file, filename: 'west_mount_photo3.jpg', content_typ
 westmount.save!
 puts "Westmount saved..."
 
-lachinecanal = Trail.new(name: "Lachine Canal Multipurpose Path", location: 'Lachine', distance: 6.6, user_id: apu.id, difficulty: 2, trail_img_url: "https://res.cloudinary.com/davidshin0413/image/upload/v1638222834/image_hyaode.png", description: "Lachine Canal Multipurpose Path is a 6.6 kilometer heavily trafficked loop trail that features a river. The trail is good for all skill levels and offers a number of activity options.")
+lachinecanal = Trail.new(name: "Lachine Canal Multipurpose Path", location: 'Lachine', distance: 6.6, user_id: chung.id, difficulty: 2, trail_img_url: "https://res.cloudinary.com/davidshin0413/image/upload/v1638222834/image_hyaode.png", description: "Lachine Canal Multipurpose Path is a 6.6 kilometer heavily trafficked loop trail that features a river. The trail is good for all skill levels and offers a number of activity options.")
 file = URI.open('https://cdn-assets.alltrails.com/uploads/photo/image/41540348/extra_large_19a3cbf151fd08e2783102106f47ff84.jpg')
 lachinecanal.photos.attach(io: file, filename: 'lachine_canal_photo.jpg', content_type: 'image/jpg')
 file = URI.open('https://cdn-assets.alltrails.com/uploads/photo/image/37401465/extra_large_d717141299fe5b57b686a0de153fedf3.jpg')
@@ -112,18 +112,18 @@ run5 = Run.create!(trail_id: westmount.id, date: DateTime.new(2021,11,29,10,0,0)
 
 puts "Creating run participants..."
 
-run1_participant1 = RunParticipant.create!(user_id: apu.id, run_id: run1.id, status: 1, creator: true)
-run1_participant2 = RunParticipant.create!(user_id: bob.id, run_id: run1.id, status: 1, creator: false)
-run1_participant3 = RunParticipant.create!(user_id: chung.id, run_id: run1.id, status: 1, creator: false)
+run1_participant1 = RunParticipant.create!(user_id: chung.id, run_id: run1.id, status: 1, creator: true)
+run1_participant2 = RunParticipant.create!(user_id: don.id, run_id: run1.id, status: 1, creator: false)
+run1_participant3 = RunParticipant.create!(user_id: emily.id, run_id: run1.id, status: 1, creator: false)
 
 puts "Creating chatrooms..."
-chatroom_1 = Chatroom.create!(name: "general", user_id: apu.id, recipient_id: bob.id)
+chatroom_1 = Chatroom.create!(name: "general", user_id: chung.id, recipient_id: don.id)
 
 puts "Creating badges..."
 badge1 = Badge.create!(name: "Newbie", description: "Completed first run.", req_xp: 1, logo: "fas fa-baby") # baby
 badge2 = Badge.create!(name: "10k", description: "Ran 10km to date.", req_xp: 10, logo: "fas fa-shoe-prints") # shoe print
-badge3 = Badge.create!(name: "Cardio", description: "Ran 10km in a single run.", req_xp: 20, logo: "fas fa-heart") # heart
-# badge4 = Badge.create!(name: "Endurance", description: "Completed 5 uphill runs of at least 1km each.", req_xp: 20, logo: "fas fa-dumbbell") # dumbbell
+badge3 = Badge.create!(name: "Cardio", description: "Ran 20km to date.", req_xp: 20, logo: "fas fa-heart") # heart
+badge4 = Badge.create!(name: "Endurance", description: "Completed 5 uphill runs of at least 1km each.", req_xp: 20, logo: "fas fa-dumbbell") # dumbbell
 badge5 = Badge.create!(name: "Social", description: "Connected with 100 fellow runners.", req_xp: 25, logo: "fas fa-handshake") # handshake
 badge6 = Badge.create!(name: "Team spirit", description: "Received 100 thumbs-up.", req_xp: 50, logo: "fas fa-thumbs-up") # thumbs-up
 badge7 = Badge.create!(name: "Lightning", description: "Ran 5km in less than 20 minutes.", req_xp: 100, logo: "fas fa-bolt") # bolt
